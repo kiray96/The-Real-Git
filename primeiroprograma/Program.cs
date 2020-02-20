b90ed8d5e2ceb65fedf99445ca89cac45cc9bd6d
Ines Barradas <inesmartinsbarradas1996@gmail.com>
2020-02-20 12:15:38 +0000

    First Commit

---
+++ primeiroprograma/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace primeiroprograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bisnaga");
+        }
+    }
+}
